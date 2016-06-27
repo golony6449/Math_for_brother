ab830256e92fd86e30924bc30ec2396b79d8859c
golony6449 <bb0162@live.co.kr>
2016-06-27 22:59:49 +0900

    develop base program
    1. change infinite loop to 10 times loop
    2. add 나눗셈 case

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ from random import randint
 import calc
 
 print "수학 문제 연습"
-print "귀찮고 힘들어도 힘내서 한번 풀어 봅시다. \n"
+print "귀찮고 힘들어도 힘내서 10문제만 풀어 봅시다. \n"
 
 symbol=raw_input("연습할 문제 유형을 선택하세요.(덧셈,뺄셈,곱셈,나눗셈)")
 level=int(raw_input("난이도를 숫자로 입력해주세요.(예를들어 1)"))
 
 if symbol=="덧셈":
-    while True:
+    for x in range(10):
         calc.plus(level)
 elif symbol=="뺄셈":
-    while True:
+    for x in range(10):
         calc.minus(level)
 elif symbol=="곱셈":
-    while True:
+    for x in range(10):
         calc.multi(level)
-elif symbol==(level):
-    while True:
+elif symbol=='나눗셈':
+    for x in range(10):
         calc.divine(level)
 else:
     print "올바르지 않은 유형입니다."
