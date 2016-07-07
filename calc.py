abbd0deebd6ea6588224a151ac8b09700fd77691
golony6449 <bb0162@live.co.kr>
2016-07-07 22:37:40 +0900

    + step development almost complete

--- calc.py
+++ calc.py
@@ -9,13 +9,12 @@ class CALC(object):
 
     def __init__(self):
         self.initial_set(textlabel='난이도를 입력해주세요. (1~)', answertext='난이도: ',para=1)
+        self.gui.mainloop()
 
     def start(self):
-        print self.level
         self.initial_set(answertext='정답 :')
-        q=self.make_Q(self.level)
-        self.set(q)
-
+        q=self.make_Q()
+        self.gui.mainloop()
 
     def initial_set(self,title='수학문제연습',textlabel='생성중...',answertext='',buttontext='확인',para=0):
         self.gui = Tkinter.Tk()
@@ -26,7 +25,6 @@ class CALC(object):
         self.bottom_frame.pack(side='bottom')
 
         #문제 부분
-        self.level = 0
         self.textlabel = Tkinter.StringVar()
         self.textlabel.set(textlabel)
         self.text = Tkinter.Label(self.up_frame, textvariable=self.textlabel)
@@ -44,12 +42,17 @@ class CALC(object):
         self.answer=Tkinter.Entry(self.bottom_frame)
         self.answer.pack(side='left')
         if para==1:
+            print 'level set'
             self.ans_button = Tkinter.Button(self.bottom_frame, textvariable=self.buttontext, command=self.set_lev)
         else:
-            self.ans_button=Tkinter.Button(self.bottom_frame,textvariable=self.buttontext,command=self.get)
+            self.ans_button=Tkinter.Button(self.bottom_frame,textvariable=self.buttontext,command=self.chk)
         self.ans_button.pack(side='left')
 
-        self.gui.mainloop()
+    def chk(self):
+        self.get()
+        if self.input==self.ans:
+            print '성공'
+            self.make_Q()
 
     def set_lev(self):
         self.level=self.get()
@@ -57,16 +60,21 @@ class CALC(object):
 
     def get(self):
         self.input=int(self.answer.get())
+        return self.input
 
-    def make_Q(self,level):
+#일단 + 기준 개발(추후수정요망)
+    def make_Q(self):
         print 'make_Q 실행!'
-        para1 = randint(10 ** level, 10 ** (level + 1))
-        para2 = randint(10 ** level, 10 ** (level + 1))
+        print self.level
+        para1 = randint(10 ** self.level, 10 ** (self.level + 1))
+        para2 = randint(10 ** self.level, 10 ** (self.level + 1))
         question = str(para1) + ' + ' + str(para2) +' = ?'
+        self.ans=para1+para2
+        self.set(question)
         return question
 
     def set(self,para):
-        self.textlabel.set('test')
+        self.textlabel.set(para)
 
     def return_para(self,level=0):
         if level==1:
